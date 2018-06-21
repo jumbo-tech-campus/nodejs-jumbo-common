59a7a5f1d55e425064b7c522f3a95b17954ac010
Geert Weggemans <geert.weggemans.cic@nl.ibm.com>
2018-06-21 11:50:56 +0200

    Add bunyan logger missing file

---
+++ src/components/hapi/bunyanTIDLogger.ts
@@ -0,0 +1,29 @@
+import * as hapi from 'hapi';
+import uuidv4 from 'uuid/v4';
+import * as Logger from 'bunyan';
+
+declare module 'hapi' {
+  interface Request {
+    tid: string;
+    logger: Logger;
+  }
+}
+
+interface BunyanHapiTIDLoggerOptions {
+  logger: Logger;
+
+}
+
+export const bunyanTIDLogger: hapi.Plugin<BunyanHapiTIDLoggerOptions> = {
+  name:     'bunyanTIDLogger',
+  register: (server, options) => {
+    server.ext('onRequest', (request, h) => {
+      request.tid    = uuidv4();
+      request.logger = options.logger.child({
+        transactionID: request.tid,
+      });
+
+      return h.continue;
+    });
+  },
+};
