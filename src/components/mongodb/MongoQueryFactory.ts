1a38249410027e31d18f7d988a9f6acbf4b74e72
Geert Weggemans <geert.weggemans.cic@nl.ibm.com>
2018-06-22 10:56:40 +0200

    add MongoCreate

--- src/components/mongodb/MongoQueryFactory.ts
+++ src/components/mongodb/MongoQueryFactory.ts
@@ -5,6 +5,7 @@ import {MongoQueryMeasurer} from './MongoQueryMeasurer';
 import {MongoFind} from './MongoFind';
 import {AsyncMeasurer} from '../statsd/AsyncMeasurer';
 import {ModelPopulateOptions} from 'mongoose';
+import {MongoCreate} from './MongoCreate';
 
 export class MongoQueryFactory<T extends mongoose.Document> {
   private readonly model: mongoose.Model<T>;
@@ -27,6 +28,16 @@ export class MongoQueryFactory<T extends mongoose.Document> {
 
   public createFindOne(findOptions: Partial<T>, populate?: ModelPopulateOptions | ModelPopulateOptions[]): MongoQuery<T | null> {
     let query = new MongoFindOne(findOptions, this.model, populate);
+
+    if (this.measurer) {
+      return new MongoQueryMeasurer(this.measurer, query);
+    }
+
+    return query;
+  }
+
+  public createCreate(createOptions: Partial<T>): MongoQuery<T> {
+    let query = new MongoCreate(createOptions, this.model);
 
     if (this.measurer) {
       return new MongoQueryMeasurer(this.measurer, query);
