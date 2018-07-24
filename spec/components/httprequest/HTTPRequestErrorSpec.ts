755bdc9a17b56c92ec6f2b5d7a52deb731893668
Geert Weggemans <geert.weggemans.cic@nl.ibm.com>
2018-07-24 14:57:20 +0200

    Fix spec

--- spec/components/httprequest/HTTPRequestErrorSpec.ts
+++ spec/components/httprequest/HTTPRequestErrorSpec.ts
@@ -4,17 +4,15 @@ import {APIError} from '../../../src/components/helpers/APIError';
 describe('A HTTPRequestError', () => {
   it('Can return a response body', () => {
     const error = new HTTPRequestError('Error');
-    const body = error.toResponseBody(false);
-    const bodyWithStack = error.toResponseBody(true);
+    const body = error.toResponseBody();
 
     expect(error.statusCode).toEqual(500);
     expect(error.name).toEqual('HTTPRequestError');
     expect(error.stack).toBeDefined();
 
-    expect(body.name).toEqual('HTTPRequestError');
+    expect(body.error).toEqual('HTTPRequestError');
     expect(body.message).toEqual('Error');
     expect(body.stack).not.toBeDefined();
-    expect(bodyWithStack.stack).toBeDefined();
 
     expect(error instanceof Error).toEqual(true);
     expect(error instanceof APIError).toEqual(true);
