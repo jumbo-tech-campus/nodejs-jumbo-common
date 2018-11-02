d757cbdc55dd9d4c94e0b938e4962ca757b755e6
Geert Weggemans <geert.weggemans.cic@nl.ibm.com>
2018-11-02 15:39:00 +0100

    Add tests for MongoQueryMeasurer

---
+++ spec/components/mongodb/MongoMeasurableSpec.ts
@@ -0,0 +1,29 @@
+import {MongoMeasurable} from '../../../src/components/mongodb/MongoMeasurable';
+import {MongoQuery} from '../../../src/components/mongodb/MongoQuery';
+
+describe('A MongoMeasurable', () => {
+  const mongoQueryMock = {
+    constructor: {
+      name: 'MongoQuery',
+    },
+    options: {},
+  } as MongoQuery<unknown>;
+
+  const mongoMeasurable = new MongoMeasurable(mongoQueryMock);
+
+  it('Can create tags for a query', () => {
+    expect(mongoMeasurable.tags).toEqual(['type:MongoQuery']);
+  });
+
+  it('Can return the query options', () => {
+    expect(mongoMeasurable.options).toBe(mongoQueryMock.options);
+  });
+
+  it('Can return the mongoQuery result', async () => {
+    mongoQueryMock.execute = () => Promise.resolve({});
+
+    const result = await mongoMeasurable.execute();
+
+    expect(result).toEqual({});
+  });
+});
