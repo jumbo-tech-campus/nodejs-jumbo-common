df8a901ab0af24d30b242327da313456363a731a
Geert Weggemans <geert.weggemans.cic@nl.ibm.com>
2018-11-26 09:22:40 +0100

    Add logging to data validator

--- spec/components/DataValidatorSpec.ts
+++ spec/components/DataValidatorSpec.ts
@@ -1,9 +1,10 @@
 import joi from 'joi';
 import {DataValidator} from '../../src/components/DataValidator';
+import {loggerMock} from '../helpers/mocks/loggerMock';
 
 describe('A DataValidator', () => {
   const joiSchema = joi.string().required();
-  const dataValidator = new DataValidator(joiSchema);
+  const dataValidator = new DataValidator(loggerMock, joiSchema);
 
   it('Can validate data', async () => {
     await dataValidator.validate('valid string').catch(() => {
--- src/components/DataValidator.ts
+++ src/components/DataValidator.ts
@@ -1,13 +1,25 @@
 import joi from 'joi';
+import * as Logger from 'bunyan';
 
 export class DataValidator {
+  private readonly logger: Logger;
   private readonly schema: joi.Schema;
 
-  public constructor(schema: joi.Schema) {
+  public constructor(logger: Logger, schema: joi.Schema) {
     this.schema = schema;
+    this.logger = logger;
   }
 
   public async validate(data: unknown): Promise<void> {
-    await joi.validate(data, this.schema);
+    try {
+      await joi.validate(data, this.schema);
+    } catch (error) {
+      this.logger.error({
+        data:  data,
+        error: error,
+      }, 'Data Validation Error');
+
+      throw error;
+    }
   }
 }
