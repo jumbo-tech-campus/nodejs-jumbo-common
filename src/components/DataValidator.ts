0535623aa1fd08108222b496eaa41b3360aeac2c
Geert Weggemans <geert.weggemans.cic@nl.ibm.com>
2018-12-05 09:20:30 +0100

    Add metrics to data validation

--- src/components/DataValidator.ts
+++ src/components/DataValidator.ts
@@ -1,12 +1,15 @@
 import joi from 'joi';
 import * as Logger from 'bunyan';
+import {StatsD} from 'hot-shots';
 
 export class DataValidator<T = any> {
   private readonly logger: Logger;
+  private readonly statsD: StatsD;
   private readonly schema: joi.Schema;
 
-  public constructor(logger: Logger, schema: joi.Schema) {
+  public constructor(logger: Logger, statsD: StatsD, schema: joi.Schema) {
     this.schema = schema;
+    this.statsD = statsD;
     this.logger = logger;
   }
 
@@ -17,6 +20,8 @@ export class DataValidator<T = any> {
         data:  data,
         error: result.error,
       }, 'Data Validation Error');
+
+      this.statsD.increment('data_validation');
 
       throw result.error;
     }
