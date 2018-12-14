89240ee6632134f644b57dfb50873487d5c47eb6
Stephanie Verlingo <stephanie.verlingo-cic.netherlands@ibm.com>
2018-12-14 10:46:03 +0100

    FOOD: Add pagination controller and tests

---
+++ spec/components/pagination/PaginationControllerSpec.ts
@@ -0,0 +1,34 @@
+import {HTTPResponse} from '../../../src/components/httprequest/HTTPResponse';
+import {Pageable, PaginationController} from '../../../src/components/pagination/PaginationController';
+
+describe('A PaginationController', () => {
+  let pageableMock           = {} as Pageable;
+  const paginationController = new PaginationController(pageableMock);
+  let httpResponse: HTTPResponse;
+
+  describe('Returning a status code 200', () => {
+    beforeEach(() => {
+      httpResponse               = {statusCode: 200} as HTTPResponse;
+      pageableMock.getPage       = () => Promise.resolve(httpResponse);
+      pageableMock.getTotalCount = () => Promise.resolve(7);
+    });
+
+    it('Returns response with pagination', async () => {
+      const response = await paginationController.get();
+      expect(Object.keys(response.body!)).toEqual(['pagingData', 'data']);
+    });
+  });
+
+  describe('Returning an error', () => {
+    beforeEach(() => {
+      httpResponse               = {} as HTTPResponse;
+      pageableMock.getPage       = () => Promise.resolve(httpResponse);
+      pageableMock.getTotalCount = () => Promise.resolve(7);
+    });
+
+    it('Returns original HTTP response', async () => {
+      const response = await paginationController.get();
+      expect(response).toBe(httpResponse);
+    });
+  });
+});
