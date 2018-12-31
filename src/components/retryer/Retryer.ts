2ccee6f87aabda105cc55b16df8c237194ac7ffa
Geert Weggemans <geert.weggemans.cic@nl.ibm.com>
2018-12-31 11:55:21 +0100

    Add defaultTags to retryer

--- src/components/retryer/Retryer.ts
+++ src/components/retryer/Retryer.ts
@@ -8,8 +8,15 @@ export class Retryer {
   private readonly retryable: Retryable;
   private readonly backo: any;
   private readonly maxAttempts: number;
+  private readonly defaultTags: string[];
 
-  public constructor(statsD: StatsD, retryable: Retryable, maxAttempts: number, minInterval: number, maxInterval: number, jitter: number) {
+  public constructor(statsD: StatsD,
+                     retryable: Retryable,
+                     maxAttempts: number,
+                     minInterval: number,
+                     maxInterval: number,
+                     jitter: number,
+                     defaultTags: string[] = []) {
     this.statsD      = statsD;
     this.retryable   = retryable;
     this.backo       = new backo({
@@ -18,12 +25,13 @@ export class Retryer {
       jitter: jitter,
     });
     this.maxAttempts = maxAttempts - 1;
+    this.defaultTags = defaultTags;
   }
 
   public async execute(): Promise<void> {
     if (await this.shouldAttempt()) {
       const attempt = this.backo.attempts + 1;
-      this.statsD.increment(`retry`, 1, this.retryable.tags.concat(`retryAttempt:${attempt}`));
+      this.statsD.increment(`retry`, 1, this.retryable.tags.concat(`retryAttempt:${attempt}`, ...this.defaultTags));
 
       await this.wait(this.backo.duration());
 
--- src/components/retryer/RetryerFactory.ts
+++ src/components/retryer/RetryerFactory.ts
@@ -9,16 +9,18 @@ export class RetryerFactory {
   private readonly minInterval: number;
   private readonly maxInterval: number;
   private readonly jitter: number;
+  private readonly defaultTags?: string[];
 
-  public constructor(statsD: StatsD, maxAttempts: number, minInterval: number, maxInterval: number, jitter: number) {
+  public constructor(statsD: StatsD, maxAttempts: number, minInterval: number, maxInterval: number, jitter: number, defaultTags?: string[]) {
     this.statsD      = statsD;
     this.maxAttempts = maxAttempts;
     this.minInterval = minInterval;
     this.maxInterval = maxInterval;
     this.jitter      = jitter;
+    this.defaultTags = defaultTags;
   }
 
   public create(retryable: Retryable & Measurable<any>): Retryer {
-    return new Retryer(this.statsD, retryable, this.maxAttempts + 1, this.minInterval, this.maxInterval, this.jitter);
+    return new Retryer(this.statsD, retryable, this.maxAttempts + 1, this.minInterval, this.maxInterval, this.jitter, this.defaultTags);
   }
 }
