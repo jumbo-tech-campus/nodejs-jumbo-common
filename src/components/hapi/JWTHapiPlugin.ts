236e2d0853aee400f1621847728f4bf58a11ec49
Stephanie Verlingo <stephanie.verlingo-cic.netherlands@ibm.com>
2019-01-29 09:37:32 +0100

    FOOD: Update error message to match UX-API

--- src/components/hapi/JWTHapiPlugin.ts
+++ src/components/hapi/JWTHapiPlugin.ts
@@ -32,7 +32,7 @@ export const createJWTUnpacker = (options: JWTHapiPluginOptions): hapi.Lifecycle
         error: error,
       }, 'Error while decoding JWT');
 
-      throw Boom.unauthorized(`Invalid JWT Token for header ${options.jwtHeader}`);
+      throw Boom.unauthorized(`Invalid Authentication Tokens`);
     }
 
     if (typeof decodedObject === 'object' && decodedObject.CustomerID) {
