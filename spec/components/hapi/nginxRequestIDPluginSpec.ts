24d7b9430bb9c3d39082bdbc034380df78d77ea3
Geert Weggemans <geert.weggemans.cic@nl.ibm.com>
2019-03-21 15:13:32 +0100

    JMBMOB-6501: Add unit tests for nginxRequestIDPlugin

--- spec/components/hapi/nginxRequestIDPluginSpec.ts
+++ spec/components/hapi/nginxRequestIDPluginSpec.ts
@@ -1,6 +1,7 @@
 import * as hapi from 'hapi';
 import {Writeable} from '../../../components/Writeable';
-import {onRequestNginxRequestIDLifecycleMethod} from '../../../src/components/hapi/nginxRequestIDPlugin';
+import {onRequestNginxRequestIDLifecycleMethod, onResponseNginxRequestIDLifecycleMethod} from '../../../src/components/hapi/nginxRequestIDPlugin';
+import Boom from 'boom';
 
 describe('A onRequestNginxRequestIDLifecycleMethod', () => {
   let requestMock: hapi.Request & any;
@@ -18,14 +19,54 @@ describe('A onRequestNginxRequestIDLifecycleMethod', () => {
     hMock.continue = Symbol('continue');
   });
 
-  describe('When creating info from a request', () => {
-    it('Sets the info on the application state', () => {
+  describe('When adding request id from a request', () => {
+    beforeEach(() => {
       onRequestNginxRequestIDLifecycleMethod(requestMock, hMock);
+    });
 
+    it('Sets the request id on the application state', () => {
+      expect(requestMock.app.requestID).toEqual('requestID');
+    });
+
+    it('Sets the request id on the request info', () => {
       expect(requestMock.app.requestInfo).toEqual({
         request_id: 'requestID',
       });
-      expect(requestMock.app.requestID).toEqual('requestID');
+    });
+  });
+});
+
+describe('A onResponseNginxRequestIDLifecycleMethod', () => {
+  let requestMock: hapi.Request & any;
+  let hMock: Writeable<hapi.ResponseToolkit>;
+
+  beforeEach(() => {
+    requestMock               = {} as hapi.Request;
+    requestMock.response      = {} as hapi.ResponseObject;
+    requestMock.app           = {};
+    requestMock.app.requestID = 'requestID';
+
+    hMock          = {} as Writeable<hapi.ResponseToolkit>;
+    hMock.continue = Symbol('continue');
+  });
+
+  describe('When no response is set', () => {
+    it('Returns h.continue', () => {
+      requestMock.response = undefined;
+
+      expect(onResponseNginxRequestIDLifecycleMethod(requestMock, hMock)).toEqual(hMock.continue);
+    });
+  });
+
+  describe('When response is error', () => {
+    beforeEach(() => {
+      requestMock.response = Boom.internal();
+
+      onResponseNginxRequestIDLifecycleMethod(requestMock, hMock);
+    });
+
+    it('Sets request id as header', () => {
+      expect(requestMock.response.output.headers['x-request-id']).toEqual('requestID');
     });
   });
 });
