f2b6a001ee8dd29d9ebd0b76e6f6fdb1c6a2460a
Geert Weggemans <geert.weggemans.cic@nl.ibm.com>
2019-04-08 14:01:41 +0200

    Add logging to http request telemetry

--- src/components/httprequest/HTTPRequestTelemetry.ts
+++ src/components/httprequest/HTTPRequestTelemetry.ts
@@ -2,24 +2,37 @@ import {HTTPRequestResponse} from './HTTPRequest';
 import {HTTPRequestDecorator} from './HTTPRequestDecorator';
 import {AsyncTelemetry} from '../telemetry/AsyncTelemetry';
 import {HTTPRequestMeasurable} from './HTTPRequestMeasurable';
+import * as Logger from 'bunyan';
 
 export class HTTPRequestTelemetry extends HTTPRequestDecorator {
-  public readonly request: HTTPRequestMeasurable;
+  public readonly executable: HTTPRequestMeasurable;
 
+  private readonly logger: Logger;
   private readonly asyncTelemetry: AsyncTelemetry;
   private readonly defaultTags?: string[];
 
   public constructor(request: HTTPRequestMeasurable,
+                     logger: Logger,
                      asyncTelemetry: AsyncTelemetry,
                      defaultTags?: string[]) {
     super(request);
 
-    this.request        = request;
+    this.executable     = request;
+    this.logger         = logger;
     this.asyncTelemetry = asyncTelemetry;
     this.defaultTags    = defaultTags;
   }
 
-  public execute(): Promise<HTTPRequestResponse> {
-    return this.asyncTelemetry.execute(this.request, this.defaultTags);
+  public async execute(): Promise<HTTPRequestResponse> {
+    const response = await this.asyncTelemetry.execute(this.executable, this.defaultTags);
+
+    if (response.statusCode >= 400) {
+      this.logger.warn({
+        request:  this.options,
+        response: response,
+      }, 'Unsuccessful HTTP Response');
+    }
+
+    return response;
   }
 }
